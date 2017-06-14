2c1d8ef1a108f91c65d842d6c1fc8d330c54054d
albpal <anorkbcn@gmail.com>
2017-06-14 11:38:36 +0200

    Some refactoring...

--- test/TestSearchEngine.js
+++ test/TestSearchEngine.js
@@ -4,7 +4,7 @@ var Contract = require('../contract');
 var assert = require('assert');
 
 describe('SearchEngine unitary testing...', function() {
-	it('should return the contract 0xD3a33636677fFF0BE34EC503b9E848b296E0972A', function () {	
+	it('Testing getAllContractsFromFilter function...', function () {	
 		var contract1 = new Contract("0xD3a33636677fFF0BE34EC503b9E848b296E0972A");
 		contract1.addProperties('type=temperature,location=barcelona');
 		var contract2 = new Contract("0xD3a33636677fFF0BE34EC503b9E848b296E0973A");
@@ -12,11 +12,10 @@ describe('SearchEngine unitary testing...', function() {
 		var contractBag = new ContractBag();
 		contractBag.addContract(contract1);
 		contractBag.addContract(contract2);
-	
+
 		var searchEngine = new SearchEngine(contractBag);
 		var contracts = searchEngine.getAllContractsFromFilter('type=temperature,location=barcelona');
 		assert.equal(contracts.length, 1, 'There is not only 1 contract');
 		assert.equal(contracts[0].getAddress(), '0xD3a33636677fFF0BE34EC503b9E848b296E0972A', 'Incorrect contract returned');
 	});
 });
-
