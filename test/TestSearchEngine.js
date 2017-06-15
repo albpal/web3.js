f8dfeb8894cc36d7a82f84a435567cb6cb318743
albpal <anorkbcn@gmail.com>
2017-06-15 09:14:51 +0200

    From contractRepository to contractRepositoryOnMemory...

--- test/TestSearchEngine.js
+++ test/TestSearchEngine.js
@@ -1,5 +1,5 @@
 var SearchEngine = require('../searchEngine');
-var ContractRepository = require('../contractRepository');
+var ContractRepositoryOnMemory = require('../contractRepositoryOnMemory');
 var Contract = require('../contract');
 var assert = require('assert');
 
@@ -9,13 +9,26 @@ describe('SearchEngine unitary testing...', function() {
 		contract1.addProperties('type=temperature,location=barcelona');
 		var contract2 = new Contract("0xD3a33636677fFF0BE34EC503b9E848b296E0973A");
 		contract2.addProperties('type=temperature,location=madrid');
-		var contractRepository = new ContractRepository();
-		contractRepository.addContract(contract1);
-		contractRepository.addContract(contract2);
+		var contractRepositoryOnMemory = new ContractRepositoryOnMemory();
+		contractRepositoryOnMemory.addContract(contract1);
+		contractRepositoryOnMemory.addContract(contract2);
 
-		var searchEngine = new SearchEngine(contractRepository);
+		var searchEngine = new SearchEngine(contractRepositoryOnMemory);
 		var contracts = searchEngine.getAllContractsFromFilter('type=temperature,location=barcelona');
 		assert.equal(contracts.length, 1, 'There is not only 1 contract');
 		assert.equal(contracts[0].getAddress(), '0xD3a33636677fFF0BE34EC503b9E848b296E0972A', 'Incorrect contract returned');
+	});
+	it('Testing assignPropertyToContract function...', function () {
+		var contract_address = "0xD3a33636677fFF0BE34EC503b9E848b296E0972A";
+		var contract1 = new Contract(contract_address);
+
+		var contractRepositoryOnMemory = new ContractRepositoryOnMemory();
+		contractRepositoryOnMemory.addContract(contract1);
+
+		var searchEngine = new SearchEngine(contractRepositoryOnMemory);
+		searchEngine.addProperties(contract_address,'type=temperature,location=barcelona');
+
+		assert.deepEqual(searchEngine.getProperties(contract_address)["type"], "temperature", 'The contract has not the same value for type ' + searchEngine.getProperties(contract_address)["type"]);
+		assert.deepEqual(searchEngine.getProperties(contract_address)["location"], "barcelona", 'The contract has not the same value for location ' + searchEngine.getProperties(contract_address)["location"]);
 	});
 });
