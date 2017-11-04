f8c4de5dcd4f47e568d208082c704e9b66185745
Albert Palau <anorkbcn@gmail.com>
2017-11-04 16:57:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Web3 = require('web3');
 var web3 = new Web3();
 
 // docker run -it -p 8545:8545 -p 30303:30303 ethereum/client-go --rpc --rpcaddr "0.0.0.0"
-web3.setProvider(new web3.providers.HttpProvider('http://37.247.52.181:8780'));
+web3.setProvider(new web3.providers.HttpProvider('http://XX.XX.XX.XX:8780'));
 
 var coinbase = "0xb794f5ea0ba39494ce839613fffba74279579268";
 var balance = web3.eth.getBalance(coinbase);
